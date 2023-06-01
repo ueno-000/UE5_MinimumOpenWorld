fad7de3b4a8f84e8c74611ea7c5f12c84604a11e
ueno-000 <sarasa4869@zoho.com>
2023-06-01 15:49:40 +0900

    [Add] 最初のコミット

---
+++ Source/MinimumOpenWorld/MinimumOpenWorldGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MinimumOpenWorldGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMinimumOpenWorldGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMinimumOpenWorldGameMode();
+};
+
+
+
